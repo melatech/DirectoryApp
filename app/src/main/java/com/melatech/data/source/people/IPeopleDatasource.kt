76d371f1013a53c115f9789ee162e5293f826a97
melatech <maincontractor@gmail.com>
2023-03-01 19:27:43 +0000

    Implement hilt for API module dependency injection

--- app/src/main/java/com/melatech/data/source/people/IPeopleDatasource.kt
+++ app/src/main/java/com/melatech/data/source/people/IPeopleDatasource.kt
@@ -1,4 +1,9 @@
 package com.melatech.data.source.people
 
+import com.melatech.data.source.remote.model.people.PeopleAPIResponse
+import retrofit2.Response
+
 interface IPeopleDatasource {
+
+   suspend fun getPeopleDirectory(): Response<PeopleAPIResponse>
 }
---
+++ app/src/main/java/com/melatech/di/ApiModule.kt
@@ -0,0 +1,35 @@
+package com.melatech.di
+
+import com.melatech.BuildConfig
+import com.melatech.data.source.remote.api.DirectoryApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDirectoryApi(retrofit: Retrofit): DirectoryApi{
+        return retrofit.create(DirectoryApi::class.java)
+    }
+
+
+
+}
